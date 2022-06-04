b57bd5cb956358b201fa36f1ac4ae65b904ff006
David Gonzalez Leon <david.gonzalez@citycable.ch>
2022-06-04 10:59:35 +0200

    debut setup.sh

--- raspberrypi/install/setup.sh
+++ raspberrypi/install/setup.sh
@@ -1,1 +1,15 @@
-
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y dnsmasq hostapd
+sudo systemctl stop dnsmasq
+sudo systemctl stop hostapd
+sudo cp dhcpcd_wifi.conf /etc/dhcpcd.conf
+sudo service dhcpcd restart
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo cp dnsmasq.conf /etc/dnsmasq.conf
+sudo systemctl start dnsmasq
+sudo cp hostapd_wifi.conf /etc/hostapd/hostapd.conf
+sudo cp hostapd_wifi /etc/default/hostapd
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl start hostapd
