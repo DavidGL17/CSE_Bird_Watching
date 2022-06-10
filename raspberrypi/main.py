6ee69090c5e0f9e9b3d83750bca5d80f8d1d9489
David Gonzalez Leon <david.gonzalez@citycable.ch>
2022-06-10 13:46:17 +0200

    small fix

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -45,7 +45,7 @@ def readFromFile():
 
 # Récupère l'image du esp
 def getPicture(ipAddr):
-   driver = webdriver.Chrome
+   driver = webdriver.Chrome(executable_path = '/usr/lib/chromium-browser/chromium-browser')   
    driver.get(ipAddr)
    # click on capture
    button_element = driver.find_element_by_id('capture')
