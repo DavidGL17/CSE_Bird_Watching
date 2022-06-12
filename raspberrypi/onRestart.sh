503e6b575a0bb3788e96aa9436e9ae45de46feb0
David Gonzalez Leon <david.gonzalez@citycable.ch>
2022-06-12 15:55:55 +0200

    final fix onRestart

--- raspberrypi/onRestart.sh
+++ raspberrypi/onRestart.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+sleep 20
 caddy stop
-caddy run --config /home/reds/cse/raspberrypi/caddy/Caddyfile
+caddy run --config /home/reds/cse/raspberrypi/caddy/Caddyfile&
 sh /home/reds/cse/raspberrypi/python.sh
